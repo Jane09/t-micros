03cc92e116b709dbcd908ade5f76e7339bca2d45
zhoujian <zhoujian@newtank.cn>
2018-12-26 13:11:27 +0800

    t-micros

--- service-hi/src/main/java/com/tmicros/ServiceHiApplication.java
+++ service-hi/src/main/java/com/tmicros/ServiceHiApplication.java
@@ -1,16 +1,20 @@
 package com.tmicros;
 
 import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.sleuth.sampler.ProbabilityBasedSampler;
 import org.springframework.cloud.sleuth.sampler.SamplerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,6 +29,8 @@ import java.util.logging.Logger;
 @EnableEurekaClient
 @RestController
 @Slf4j
+@EnableHystrix
+@EnableHystrixDashboard
 public class ServiceHiApplication {
 
     private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
@@ -53,7 +59,17 @@ public class ServiceHiApplication {
     public String info(){
         LOG.log(Level.INFO, "calling trace service-hi ");
         return "i'm service-hi";
+    }
 
+    @RequestMapping("/fallback")
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String home(@RequestParam String name) {
+        throw new RuntimeException("hhh");
+//        return "hi "+name+",i am from port:" +port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 
     @Bean
